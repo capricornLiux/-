fe4eba98e0c557cafb331e1385d9847cd95ab9a5
liuxin <784532927@qq.com>
2018-04-17 15:57:10 +0800

    三级缓存中的二级缓存

--- app.js
+++ app.js
@@ -30,39 +30,61 @@ var server = http.createServer(function (request, response) {
         response.setHeader('Cache-Control', `max-age= ${config.maxAge}`);
     }
 
-    // 判断请求文件是否存在
-    fs.exists(realPath, function (exist) {
-        if (!exist) {
-            // 不存在
-            // 返回 404 状态码
-            response.writeHead(404, {
-                'Content-Type': 'text/plain'
-            })
-            response.write('This file not fount');
+    // 获取文件的最后修改时间
+    console.log(realPath);
+    if (realPath == 'assets/favicon.ico') {
+        return;
+    }
+    fs.stat(realPath, function (err, stats) {
+        console.log(stats);
+        // 从fs.stats对象上获取最后修改时间, 并转化为utc时间
+        var lastModified = stats.mtime.toUTCString();
+        response.setHeader('Last-Modified', lastModified);
+
+        // 判断请求中是不是有if-modified-since头
+        if (request.headers['if-modified-since'] && lastModified === request.headers['if-modified-since']) {
+            console.log('same')
+            response.writeHead(304, 'Not Modified')
+            // 直接返回
             response.end()
         } else {
-            // 存在
-            // 读取文件
-            fs.readFile(realPath, 'binary', function (err, data) {
-                if (err) {
-                    // 返回错误
-                    response.writeHead(500, {
+            fs.exists(realPath, function (exist) {
+                if (!exist) {
+                    // 不存在
+                    // 返回 404 状态码
+                    response.writeHead(404, {
                         'Content-Type': 'text/plain'
                     })
-                    response.write('Server inner error');
-                    response.end();
+                    response.write('This file not fount');
+                    response.end()
                 } else {
-                    // 没有错误
-                    // 返回正确的状态码
-                    response.writeHead(200, {
-                        'Content-Type': 'image/jpeg'
+                    // 存在
+                    // 读取文件
+                    fs.readFile(realPath, 'binary', function (err, data) {
+                        if (err) {
+                            // 返回错误
+                            response.writeHead(500, {
+                                'Content-Type': 'text/plain'
+                            })
+                            response.write('Server inner error');
+                            response.end();
+                        } else {
+                            // 没有错误
+                            // 返回正确的状态码
+                            response.writeHead(200, {
+                                'Content-Type': 'image/jpeg'
+                            })
+                            // 将文件输出
+                            response.write(data, 'binary')
+                            response.end();
+                        }
                     })
-                    // 将文件输出
-                    response.write(data, 'binary')
-                    response.end();
                 }
             })
         }
+
+            // 判断请求文件是否存在
+
     })
 })
 
